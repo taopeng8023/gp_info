3cab0dabc254cb6e2d26a64fd0c46084fe18d2d2
peng.tao <peng.tao@opay-inc.com>
2024-01-10 18:43:35 +0800

    thread

--- akshare_get_gp_his_info.py
+++ akshare_get_gp_his_info.py
@@ -2,6 +2,8 @@ import akshare as ak
 import pymysql
 import datetime
 import numpy as np
+import threading
+from threading import current_thread
 
 #数据库设置
 def getDb():
@@ -22,7 +24,7 @@ def getCurrentDay():
    return formatter_day
 
 #获取所有A+H股票代码
-def getGpCode():
+def getGpCode(startTime,endTime):
     _gpCodeData = ak.stock_zh_a_spot_em()
     _gpCodeLeng = _gpCodeData.shape[0]
     for i in range(_gpCodeLeng):
@@ -34,7 +36,7 @@ def getGpCode():
         _latest_price = _gpData['最新价']
         print(_xuhao,":",_gp_code,":",_gp_name)
         if not np.isnan(_latest_price):
-            getGpHisInfo(_gp_code, _gp_name, '20240101', '20240109')
+            getGpHisInfo(_gp_code, _gp_name, startTime, endTime)
 
 def getGpHisInfo(gpCode,gpName,start_time,end_time):
     _gpHisData = ak.stock_zh_a_hist(symbol=gpCode,period='daily',start_date=start_time,end_date=end_time,adjust='hfq')
@@ -57,9 +59,31 @@ def getGpHisInfo(gpCode,gpName,start_time,end_time):
             db = getDb()
             cursor = db.cursor()
             cursor.execute(
-                f"insert into gp_info (market_time, gp_code,gp_name,latest_price,rise_fall_rang,rise_fall_amount,turnover,transaction_volume,amplitude,highest_price,minimum_price,today_open_price,turnover_rate) "
+                f"insert into gp_info_1 (market_time, gp_code,gp_name,latest_price,rise_fall_rang,rise_fall_amount,turnover,transaction_volume,amplitude,highest_price,minimum_price,today_open_price,turnover_rate) "
                 f"value ('{_marketTimeNew}','{gpCode}','{gpName}',{_latest_price},{_rise_fall_rang},{_rise_fall_amount},{_turnover},{_transaction_volume},{_amplitude},{_highest_price},{_minimum_price},{_today_open_price},{_turnover_rate});")
             cursor.rowcount
             db.commit()
+class Thread1(threading.Thread):
+    def run(self):
+        getGpCode('20231201', '20231210')
 
-getGpCode()
+class Thread2(threading.Thread):
+    def run(self):
+        getGpCode('20231211','20231220')
+
+class Thread3(threading.Thread):
+    def run(self):
+        getGpCode('20231221','20231231')
+
+def _main():
+    t1 = Thread1()
+    t2 = Thread2()
+    t3 = Thread3()
+    t1.start()
+    t2.start()
+    t3.start()
+
+
+
+_main()
+
